e8c7aaef90c5f166dc80d49c1b53e473d82aa150
Paul Campbell <paulgcampbell@gmail.com>
2013-07-24 08:11:20 +0100

    initial commit

---
+++ VerbalExpressions.Tests/BasicMatching.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Shouldly;
+
+namespace VerbalExpressions.Tests
+{
+    [TestFixture]
+    public class BasicMatching
+    {
+        [Test]
+        public void Matches_StartsWith_Positive()
+        {
+            var urlRegex = new VerbalExpression()
+            .StartOfLine()
+            .Then("http")
+            .Anything()
+            .EndOfLine();
+
+            var url = "http://www.google.com";
+
+            urlRegex.Test(url).ShouldBe(true);
+        }
+
+        [Test]
+        public void Matches_StartsWith_Negative()
+        {
+            var urlRegex = new VerbalExpression()
+            .StartOfLine()
+            .Then("http")
+            .Anything()
+            .EndOfLine();
+
+            var url = "not a url";
+
+            urlRegex.Test(url).ShouldBe(false);
+        }
+
+
+        [Test]
+        public void Matches_Maybe_Positive()
+        {
+            var urlRegex = new VerbalExpression()
+            .StartOfLine()
+            .Then("http")
+            .Maybe("s")
+            .Then("://")
+            .Anything()
+            .EndOfLine();
+
+            var withoutMaybeUrl = "http://www.google.com";
+            urlRegex.Test(withoutMaybeUrl).ShouldBe(true);
+
+            var withMaybeUrl = "https://www.google.com";
+            urlRegex.Test(withMaybeUrl).ShouldBe(true);
+        }
+
+        [Test]
+        public void Matches_Maybe_Negative()
+        {
+            var urlRegex = new VerbalExpression()
+            .StartOfLine()
+            .Then("http")
+            .Maybe("s")
+            .Then("://")
+            .Anything()
+            .EndOfLine();
+
+            var withMaybeUrl = "httpT://www.google.com";
+            urlRegex.Test(withMaybeUrl).ShouldBe(false);
+        }
+    }
+}
---
+++ VerbalExpressions/VerbalExpression.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace VerbalExpressions
+{
+    public class VerbalExpression
+    {
+        private string _regex;
+
+        public VerbalExpression()
+        {
+            _regex = "";
+        }
+
+        public VerbalExpression StartOfLine()
+        {
+            _regex = "^";
+            return this;
+        }
+
+        public VerbalExpression Then(string match)
+        {
+            _regex = _regex + match;
+            return this;
+        }
+
+        public VerbalExpression Anything()
+        {
+            _regex = _regex + "(.|\n)*";
+            return this;
+        }
+
+        public VerbalExpression Maybe(string optionalPart)
+        {
+            _regex = _regex + string.Format("({0})?", optionalPart);
+            return this;
+        }
+
+        public VerbalExpression EndOfLine()
+        {
+            return this;
+        }
+
+        public Boolean Test(string stringToTest)
+        {
+            var regex = new Regex(_regex);
+            var match = regex.Match(stringToTest);
+            return match.Success;
+        }
+    }
+}
